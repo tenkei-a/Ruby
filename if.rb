667d050ca72ffb81ef91879228ddd75a62d5a6d0
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 16:31:17 +0900

    animal

--- if.rb
+++ if.rb
@@ -1,11 +1,21 @@
-score = 90
+# score = 90
 
-if score >= 90
-    puts 'A'
-elsif score >= 80
-    puts 'B'
-elsif score >= 60
-    puts 'C'
+# if score >= 90
+#     puts 'A'
+# elsif score >= 80
+#     puts 'B'
+# elsif score >= 60
+#     puts 'C'
+# else
+#     puts 'D'
+# end
+
+animal = 'cat'
+
+if animal == 'cat'
+    puts 'にゃー'
+elsif animal == 'dog'
+    puts 'ワン'
 else
-    puts 'D'
+    puts '鳴き声が未登録です'
 end
