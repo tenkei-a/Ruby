499086e2a51b0c62ee4357f3dab0134126aa1cee
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 16:38:51 +0900

    coding case

---
+++ case.rb
@@ -0,0 +1,21 @@
+# stone = 'ruby'
+# if stone == 'ruby'
+#     puts '7月'
+# elsif stone == 'peridot'
+#     puts '8月'
+# elsif stone == 'sapphire'
+#     puts '9月'
+# else
+#     puts '未登録です'
+# end
+
+stone = 'ruby'
+
+case stone
+when 'ruby'
+    puts '7月'
+when 'peridot'
+    puts '8月'
+when 'sapphire'
+    puts '9月'
+end
