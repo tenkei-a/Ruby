4155a44b76b6c08da67ab981c7a54e084c972bf6
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 15:57:31 +0900

    many comment on ruby

--- hello.rb
+++ hello.rb
@@ -1,2 +1,14 @@
-puts 'Hello World!'
-puts 10 + 3
+# puts 'Hello World!'
+# puts 10 + 3
+
+# puts 'Hello World!'
+# puts 'Hello World!'
+# puts 'Hello World!'
+# puts 'Hello World!'
+
+# 変数の命名規則はスネークケース
+# 例:price_cost = 100
+# 定数の命名規則は基本大文字統一
+# 再代入はNG(不可能ではないがやらない)
+# 例:TAX = 1.08
+# 例:TAX_RATE = 1.08
