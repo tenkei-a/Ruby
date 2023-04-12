64c4d95fe394225ae68c31775f8b6cb1d0e79db9
tenkei-a <k1.abe0423@gmail.com>
2023-04-12 14:16:34 +0900

    create each.rb

---
+++ each.rb
@@ -0,0 +1,26 @@
+# each
+numbers = [1,2,3,4,5]
+# numbers.each do |num|
+#     puts num
+# end
+
+# numbers.each { |num| puts num }
+
+# colors = ['red','green','blue']
+# colors.each do |color|
+#     puts color
+# end
+
+# for number in numbers do
+#     puts number
+# end
+
+# scores = {nakamura: 90, sato: 80, takahashi: 100}
+# scores.each do |name, score|
+#     puts "#{name}, #{score}"
+# end
+
+# for number in numbers do
+#     puts number
+# end
+
